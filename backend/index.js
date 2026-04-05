f7b6ac79276c4f8cb6f3301d77094c5068ed5b7c
ruteshchavan <146312626+ruteshchavan@users.noreply.github.com>
2026-04-05 23:19:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://jobconnect-frontend-z95i.onrender.com",
   credentials: true,
 };
 
